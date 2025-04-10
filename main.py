9641e94a1e55ee1355ca1d0401348957cd0b127d
folefacb <folefacb@mcmaster.ca>
2025-04-09 23:21:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ ticker = st.sidebar.text_input('Ticker')
 start_date = st.sidebar.date_input('Start Date')
 end_date = st.sidebar.date_input('End Date')
 data=yf.download(ticker,start=start_date, end=end_date)
+fig = px.line(data, x=data.index, y=data['Adj Close'], title=ticker)
+st.plotly_chart(fig)
+
+
 
